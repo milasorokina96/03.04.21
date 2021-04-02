0a60d285cc10b833d1840b3a60053bcba5d2f397
Lyuda.Pik <milasorokina96@gmail.com>
2021-04-03 01:52:17 +0300

    gulp

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,63 @@
 class Chat {
-  constructor(btnSend, userSms, chatArea) {
-    this.btnSend = btnSend,
-    this.userSms = userSms,
-    this.chatArea = chatArea,
-
-    this.sendSms();
-  }
-
-  timeout(sms, time) {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(sms), time * 1000);
-    });
-  
-  }
-  
-  rand(min, max) {
-    return Math.floor(Math.random() * max + min);
-  
-  }
-  
-  createBrowserSms(userSms){
-    let browserSms;
-    if(userSms === 'Bye' || userSms === 'bye') {
-      browserSms = 'Bye';
-    } else {
-      browserSms = [
-        'Hello! I am a unicorn',
-        'Let dance',
-        'I can answer random sms',
-        'Okey',
-        'I am fine, and you?',
-      ][this.rand(0, 4)];
+    constructor(btnSend, userSms, chatArea) {
+        this.btnSend = btnSend;
+        this.userSms = userSms;
+        this.chatArea = chatArea;
+        this.sendSms();
     }
-    return this.timeout(browserSms, this.rand(1, 10));
-  }
-  
-  async showBrowserAnswer(userSms) {
-    
-    let browserSms = await this.createBrowserSms(userSms); // дождаться выполнения Таймера
-  
-    this.createSms(browserSms, 'chat__browserSms');
-  
-  }
 
-  createSms(text, classWhoseSms){
-    const sms = document.createElement('div');
-    sms.innerText = text;
-    sms.classList.add('chat__sms', classWhoseSms);
-    this.chatArea.appendChild(sms);
-    document.querySelector('.chat__sms:last-child').scrollIntoView();
-  
-  }
+    timeout(sms, time) {
+        return new Promise(resolve => {
+            const ms = 1000;
+            setTimeout(() => resolve(sms), time * ms);
+        });
+    }
 
-  sendSms() {
-  
-    this.btnSend.addEventListener('click', () => {
-  
-      if(this.userSms.value){
-        this.createSms(this.userSms.value, 'chat__userSms');
-        this.showBrowserAnswer(this.userSms.value);
-  
-      }
-      this.userSms.value = '';
-  
-    });
-  }
+    rand(min, max) {
+        return Math.floor(Math.random() * max + min);
+    }
+
+    createBrowserSms(userSms){
+        let browserSms;
+        if(userSms === 'Bye' || userSms === 'bye') {
+            browserSms = 'Bye';
+        } else {
+            browserSms = [
+                'Hello! I am a unicorn',
+                'Let dance',
+                'I can answer random sms',
+                'Okey',
+                'I am fine, and you?',
+            ][this.rand(0, 4)];
+        }
+        return this.timeout(browserSms, this.rand(1, 10));
+    }
+
+    async showBrowserAnswer(userSms) {
+        const browserSms = await this.createBrowserSms(userSms); // дождаться выполнения Таймера
+        this.createSms(browserSms, 'chat__browserSms');
+    }
+
+    createSms(text, classWhoseSms){
+        const sms = document.createElement('div');
+        sms.innerText = text;
+        sms.classList.add('chat__sms', classWhoseSms);
+        this.chatArea.appendChild(sms);
+        document.querySelector('.chat__sms:last-child').scrollIntoView();
+    }
+
+    sendSms() {
+        this.btnSend.addEventListener('click', () => {
+            if(this.userSms.value){
+                this.createSms(this.userSms.value, 'chat__userSms');
+                this.showBrowserAnswer(this.userSms.value);
+            }
+            this.userSms.value = '';
+        });
+    }
 }
-
 new Chat (
-  document.getElementById('btnSend'),
-  document.forms.chatForm.chatSms,
-  document.getElementById('chatArea'),
-
-)
+    document.getElementById('btnSend'),
+    document.forms.chatForm.chatSms,
+    document.getElementById('chatArea'),
+);
---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+// Данной строчкой мы подключаем Gulp к нашему проекту, посредством функции require. 
+// Данная функция подключает пакеты из папки node_modules в наш проект, присваивая их переменной. 
+const { src, dest, parallel, series, watch } = require('gulp');
+// npm install --save-dev gulp-babel @babel/core @babel/preset-env
+// Babel - это транспайлер, который переписывает код современного стандарта ES2015+ на более поздний
+const babel = require('gulp-babel');
+// npm install --save-dev gulp-uglify
+// минификация JS кода. позволяет сжимать и минимизировать файлы JS/CSS до 80% от их первоначального размера
+const uglify = require('gulp-uglify');
+// npm install --save-dev gulp-concat
+// Конкатенация (соеденить несколько файлов в один)
+const concat = require('gulp-concat');
+// npm install node-sass gulp-sass --save-dev
+// преобразование Sass, Scss в CSS
+const sass = require('gulp-sass');
+// npm install gulp-clean-css --save-dev
+// минификация CSS
+const mincss = require('gulp-clean-css');
+// npm install gulp-eslint -D
+const eslint = require('gulp-eslint');
+// npm i del -D
+// удаление
+const del = require('del');
+// npm i browser-sync -D
+//
+const browserSync = require('browser-sync').create();
+
+function moveHTML() {
+    return src(['./index.html']) // Берем источник
+        .pipe(dest('./build')) // Выгружаем результата в папку
+        .pipe(browserSync.reload({stream:true}));
+}
+
+function buildJS() {
+    return src('assets/js/*.js')
+        //  eslint () присоединяет вывод линта к свойству "eslint"
+        //  файлового объекта, чтобы его могли использовать другие модули.
+        .pipe(eslint())
+        //  eslint.format () выводит результаты линта на консоль.
+        .pipe(eslint.format())
+        //  Чтобы процесс завершился с кодом ошибки (1) на
+        //  lint error, возвращаем поток и конвейер для failAfterError последними.
+        .pipe(eslint.failAfterError())
+        .pipe(babel({
+            presets: ['@babel/preset-env']
+        }))
+        .pipe(uglify())
+        .pipe(concat('scripts.js'))
+    
+        .pipe(dest('build/js'))
+        .pipe(browserSync.reload({stream:true}));
+}
+
+function buildCSS() {
+    return src('assets/scss/*.scss')
+        .pipe(sass())
+        .pipe(mincss())
+        .pipe(concat('styles.css'))
+        .pipe(dest('build/style'))
+        .pipe(browserSync.reload({stream:true}));
+}
+
+function launchBrowser() {
+    browserSync.init({
+        server: {
+            baseDir: "build/"
+        }
+    });
+    watch('assets/js/*.js', buildJS,);
+    watch('assets/scss/*.scss', buildCSS);
+    watch('index.html', moveHTML);
+}
+
+function deleteBuild() {
+    return del('build');
+}
+
+exports.build = series(deleteBuild, parallel(moveHTML, buildJS, buildCSS), launchBrowser);
