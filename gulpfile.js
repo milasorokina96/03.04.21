8b5620f005f5dc1ba73fe33ea2327cf1c9d021ac
Lyuda.Pik <milasorokina96@gmail.com>
2021-04-03 04:28:50 +0300

    gulp2

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 // Данной строчкой мы подключаем Gulp к нашему проекту, посредством функции require. 
 // Данная функция подключает пакеты из папки node_modules в наш проект, присваивая их переменной. 
 const { src, dest, parallel, series, watch } = require('gulp');
+// npm install gulp-htmlmin --save-dev
+const minhtml = require('gulp-htmlmin');
 // npm install --save-dev gulp-babel @babel/core @babel/preset-env
 // Babel - это транспайлер, который переписывает код современного стандарта ES2015+ на более поздний
 const babel = require('gulp-babel');
@@ -26,8 +28,12 @@ const del = require('del');
 const browserSync = require('browser-sync').create();
 
 function moveHTML() {
-    return src(['./index.html']) // Берем источник
-        .pipe(dest('./build')) // Выгружаем результата в папку
+    return src('index.html') // Берем источник
+        .pipe(minhtml({
+            collapseWhitespace: true, // удаляем все переносы
+            removeComments: true // удаляем все комментарии
+          }))
+        .pipe(dest('build')) // Выгружаем результата в папку
         .pipe(browserSync.reload({stream:true}));
 }
 
@@ -42,7 +48,7 @@ function buildJS() {
         //  lint error, возвращаем поток и конвейер для failAfterError последними.
         .pipe(eslint.failAfterError())
         .pipe(babel({
-            presets: ['@babel/preset-env']
+            presets: ['@babel/env'] // берет любые указанные вами целевые среды и проверяет их на соответствие своим сопоставлениям, чтобы составить список плагинов и передает его Babel.
         }))
         .pipe(uglify())
         .pipe(concat('scripts.js'))
@@ -61,10 +67,8 @@ function buildCSS() {
 }
 
 function launchBrowser() {
-    browserSync.init({
-        server: {
-            baseDir: "build/"
-        }
+    browserSync.init({ //Запустит и инициализирует сервер
+        server: 'build/'
     });
     watch('assets/js/*.js', buildJS,);
     watch('assets/scss/*.scss', buildCSS);
@@ -75,4 +79,5 @@ function deleteBuild() {
     return del('build');
 }
 
+// default - gulp
 exports.build = series(deleteBuild, parallel(moveHTML, buildJS, buildCSS), launchBrowser);
